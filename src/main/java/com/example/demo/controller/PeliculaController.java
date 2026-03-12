ea1ff083ae5d39a173d862d75f99d6c4b742d4fc
franco-astudillo <fr.astudillor@duocuc.cl>
2026-03-12 20:32:16 -0300

    versiones

--- src/main/java/com/example/demo/controller/PeliculaController.java
+++ src/main/java/com/example/demo/controller/PeliculaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/peliculas")
+@RequestMapping("/api/v1/peliculas")
 public class PeliculaController {
 
     @Autowired
