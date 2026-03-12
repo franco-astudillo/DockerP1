1ce95472476ecbb2fa1a60bb52092f763d4f9e54
franco-astudillo <fr.astudillor@duocuc.cl>
2026-03-12 18:47:30 -0300

    segundo guardado

--- src/main/java/com/example/demo/controller/PeliculaController.java
+++ src/main/java/com/example/demo/controller/PeliculaController.java
@@ -4,7 +4,13 @@ package com.example.demo.controller;
 import com.example.demo.model.Pelicula;
 import com.example.demo.repository.PeliculaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/peliculas")
@@ -14,7 +20,10 @@ public class PeliculaController {
     private PeliculaRepository bookRepository;
 
     @PostMapping
-    public Pelicula createPelicula(@RequestBody Pelicula pelicula) {
-        return bookRepository.save(pelicula);
+    public ResponseEntity<Pelicula> createPelicula(@RequestBody Pelicula pelicula) {
+        Pelicula nuevaPelicula = bookRepository.save(pelicula);
+        // Esto asegura que Postman reciba el objeto creado y un estado 201
+        return new ResponseEntity<>(nuevaPelicula, HttpStatus.CREATED);
+
     }
 }
